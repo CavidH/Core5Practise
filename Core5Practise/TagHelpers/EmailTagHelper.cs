267c4ef43b3fad5791db584bb351f1c7b72be73b
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-01 14:31:57 +0400

    Custom TagHelper

---
+++ Core5Practise/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Core5Practise.TagHelpers
+{
+    public class EmailTagHelper :TagHelper
+    {
+        public string Email { get; set; }
+        public string Content   { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.Add("href",$"mailto:{Email}");
+            output.Content.Append(Content);
+        }
+    }
+}
