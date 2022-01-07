6a4effbf93008d36893590da1ba2a13ad93121b5
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-07 21:53:53 +0400

    start validation

---
+++ Core5Practise/Controllers/VL.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core5Practise.Controllers.Vl {
+    public class VL : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
