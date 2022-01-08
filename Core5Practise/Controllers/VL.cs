5927bd933cd4fd2b10e23f54c342dd04eca2a6db
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-08 13:35:25 +0400

    Validation

--- Core5Practise/Controllers/VL.cs
+++ Core5Practise/Controllers/VL.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Core5Practise.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core5Practise.Controllers.Vl {
     public class VL : Controller
     {
         // GET
-        public IActionResult Index()
+        public IActionResult CreateProduct()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                // foreach (var erorr in ModelState.Values.FirstOrDefault().Errors)
+                // {
+                //     ModelState.AddModelError("",erorr.ErrorMessage);
+                // }
+                var msg = ModelState.ToList();
+
+                return View(product);
+            }
+          
+            return Content("");
+        }
+        
     }
 }
--- Core5Practise/Models/Product.cs
+++ Core5Practise/Models/Product.cs
@@ -1,9 +1,17 @@
-﻿namespace Core5Practise.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Core5Practise.Models
 {
     public class Product
     {
+        [Required(ErrorMessage = "Please enter product name ")]
+        [StringLength(55, ErrorMessage = " please enter 55 character> Name ")]
         public string name { get; set; }
         public int quantity { get; set; }
+        [Required(ErrorMessage = "Please enter Email")]
+        [EmailAddress(ErrorMessage = "please insert valid email addres")]
+        public string Email { get; set; }
 
     }
 }
