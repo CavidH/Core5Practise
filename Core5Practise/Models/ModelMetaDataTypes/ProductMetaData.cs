d048c10d3f347f8f5ee0f4087878c89ab947c1ac
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-08 15:15:56 +0400

    metadata

---
+++ Core5Practise/Models/ModelMetaDataTypes/ProductMetaData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core5Practise.Models.ModelMetaDataTypes
+{
+    public class ProductMetaData
+    {
+        [Required(ErrorMessage = "Please enter product name ")]
+        [StringLength(55, ErrorMessage = " please enter 55 character> Name ")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "Please enter Email")]
+        [EmailAddress(ErrorMessage = "please insert valid email addres")]
+        public string Email { get; set; }
+
+    }
+}
--- Core5Practise/Models/Product.cs
+++ Core5Practise/Models/Product.cs
@@ -1,16 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.Intrinsics.Arm;
+using Core5Practise.Models.ModelMetaDataTypes;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core5Practise.Models
 {
+    
+    
+    [ModelMetadataType(typeof(ProductMetaData))]
     public class Product
     {
-        [Required(ErrorMessage = "Please enter product name ")]
-        [StringLength(55, ErrorMessage = " please enter 55 character> Name ")]
         public string name { get; set; }
         public int quantity { get; set; }
-        [Required(ErrorMessage = "Please enter Email")]
-        [EmailAddress(ErrorMessage = "please insert valid email addres")]
         public string Email { get; set; }
 
     }
